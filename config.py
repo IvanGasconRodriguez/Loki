f311ab38fc5a7959d33327ddbd182c18cd69bf4e
Ivanko1938 <159453601+Ivanko1938@users.noreply.github.com>
2026-02-04 13:59:06 +0100

    Replace GHL_API_KEY with GHL_ACCESS_TOKEN

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GHL_API_KEY = os.getenv("GHL_API_KEY")
+GHL_ACCESS_TOKEN = os.getenv("GHL_ACCESS_TOKEN")
 GHL_LOCATION_ID = os.getenv("GHL_LOCATION_ID")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
